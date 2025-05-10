55f1c99bc49935f15819680fa11eb226210de2af
nour akram <nourakram286@gmail.com>
2025-05-11 00:18:22 +0300

    fix conflict

--- src/redux/features/hotels/HotelService.js
+++ src/redux/features/hotels/HotelService.js
@@ -1,12 +1,9 @@
 import api from "../../../services/api";
 
 export const getHotels = async () => {
-<<<<<<< HEAD
   const res = await api.get("/hotels");
-=======
-  const res = await api.get("/hotel");
+
   console.log(res.data);
-  
->>>>>>> 5a00bc7d91aef1cfe559a4492b60a17b712270c5
+
   return res.data;
 };
